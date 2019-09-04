81258b66828f12797b785a470092c654c0ba3e23
envincebal <vincentyan1@gmail.com>
2019-09-03 22:03:23 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './styles/styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Find A Recipe <i class="fas fa-hamburger"></i>
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/utils/api.jsx
@@ -0,0 +1,4 @@
+const apiID = `a23e2e90`;
+const apiKey = `6220bdba80c64c71f7674f546eef3872`;
+const apiURL = `https://api.edamam.com/search?q=${searchTerm}&app_id=${apiID}&app_key=${
+  apiKey}&from=0&to=20`;
