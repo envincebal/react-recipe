be480e53b3bf08d2c7197fec7c1f6d01415114d8
envincebal <vincentyan1@gmail.com>
2019-09-13 13:24:19 -0700

    Updates

---
+++ precache-manifest.262ccb116592b20c2f37ddb969bba066.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8482d8dc247f6f1cd68b4f541acf3389",
+    "url": "/react-recipe/index.html"
+  },
+  {
+    "revision": "4f17662328fd00218b8e",
+    "url": "/react-recipe/static/css/main.2095b0a6.chunk.css"
+  },
+  {
+    "revision": "0ff444f0b2f65f4d29f6",
+    "url": "/react-recipe/static/js/2.33e02529.chunk.js"
+  },
+  {
+    "revision": "4f17662328fd00218b8e",
+    "url": "/react-recipe/static/js/main.cc4e05bc.chunk.js"
+  },
+  {
+    "revision": "444d8f5cb7f15d05cb42",
+    "url": "/react-recipe/static/js/runtime~main.0b4f817e.js"
+  }
+]);
